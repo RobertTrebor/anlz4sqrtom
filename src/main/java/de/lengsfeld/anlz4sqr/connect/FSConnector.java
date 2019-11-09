55f77266b2089c0c27e63cf969c2592a1e0a07e1
rl <robert@lengsfeld.de>
2019-11-09 09:41:23 +0100

    simple branch to test basic jsf

--- src/main/java/de/lengsfeld/anlz4sqr/connect/FSConnector.java
+++
@@ -1,9 +0,0 @@
-package de.lengsfeld.anlz4sqr.connect;
-
-import fi.foyt.foursquare.api.FoursquareApi;
-
-public interface FSConnector {
-    String authorize();
-    String authorizeToken(String header);
-    FoursquareApi getFoursquareApi();
-}
